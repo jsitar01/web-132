eb08ab986f0c4afacc67559d3548d7987d13705b
jsitar01 <sitarikjessica@hotmail.com>
2016-04-27 17:33:30 -0700

    Added files via upload
    Added JavaScript

---
+++ Web 132 - JSitarik/javascript.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+function a_message() 
+{ 
+var theAnswer = prompt("Please enter your Username", "");
+
+ if (theAnswer){
+     alert("Enjoy!");
+  }
+	
+ else{
+    alert("Error! Please enter username!");
+  }
+}
+
+
+function analyzeColor3(myColor) {
+	if (myColor == "Yes") {
+		alert("Good job, M8.");
+		}
+	else if (myColor == "No") {
+		alert("What are you waiting for?");
+	}
+	else if (myColor == "Maybe") {
+		alert("Do you even lift?");
+	}
+}
+
+
+
